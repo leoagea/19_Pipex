0be6d5c41dc6f5a6496e83667715e6c502c23fe6
leoagea <leoagea@gmail.com>
2024-06-14 13:36:25 +0200

    Add : main file and main funct written

---
+++ src/main.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 13:02:20 by lagea             #+#    #+#             */
+/*   Updated: 2024/06/14 13:35:50 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/pipex.h"
+
+static check_perm(char **av)
+{
+	if (ft_strncmp(av[2], "/dev/stdin",10) == 0 || ft_strncmp(av[2], "/dev/random",11) == 0 || ft_strncmp(av[2], "/dev/urandom",12) == 0)
+	{
+		write(2, "zsh: permission denied :",24);
+		write(2, av[2], ft_strlen(av[2]));
+		write(2, "\n",1);
+		exit(1);
+	}
+}
+int main(int ac, char **av, char **envp)
+{
+	int fd[2];
+	int id;
+	
+	if (ac < 5)
+	{
+		write(2, "Not enough arguments\n",21);
+		return 1;
+	}
+	if (ac > 5)
+	{
+		write(2, "Too much arguments\n",19);
+		return 1;
+	}
+	check_perm(av);
+	if (pipe(fd) == -1)
+	{
+		perror("zsh");
+		return 1;
+	}
+	id = fork();
+	if (id == -1)
+	{
+		perror("zsh");
+		return 1;
+	}
+	if (id == 0)
+		child_process(fd, av, envp);
+	else
+		parent_process(fd, av, envp);
+}
