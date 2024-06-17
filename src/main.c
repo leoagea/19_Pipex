aa15e931fbbc696f121767f63da2264c85570c03
leoagea <leoagea@gmail.com>
2024-06-17 12:56:24 +0200

    Fix : Norme OK

--- src/main.c
+++ src/main.c
@@ -6,54 +6,50 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 13:02:20 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/14 18:15:14 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/17 12:56:07 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
 
-static void check_perm(char **av)
+static void	check_perm(char **av)
 {
-	if (ft_strncmp(av[2], "/dev/stdin",10) == 0 || ft_strncmp(av[2], "/dev/random",11) == 0 || ft_strncmp(av[2], "/dev/urandom",12) == 0)
+	if (ft_strncmp(av[2], "/dev/stdin", 10) == 0 || ft_strncmp(av[2],
+			"/dev/random", 11) == 0 || ft_strncmp(av[2], "/dev/urandom",
+			12) == 0)
 	{
-		write(2, "zsh: permission denied :",24);
+		write(2, "zsh: permission denied :", 24);
 		write(2, av[2], ft_strlen(av[2]));
-		write(2, "\n",1);
+		write(2, "\n", 1);
 		exit(1);
 	}
 }
 
-int main(int ac, char **av, char **envp)
+int	main(int ac, char **av, char **envp)
 {
-	int fd[2];
-	int id;
-	
+	int	fd[2];
+	int	id;
+
 	if (ac < 5)
 	{
-		write(2, "Not enough arguments\n",21);
-		return 1;
+		write(2, "Not enough arguments\n", 21);
+		return (1);
 	}
 	if (ac > 5)
 	{
-		write(2, "Too much arguments\n",19);
-		return 1;
+		write(2, "Too much arguments\n", 19);
+		return (1);
 	}
 	check_perm(av);
 	if (pipe(fd) == -1)
-	{
-		perror("zsh");
-		return 1;
-	}
+		exit_error();
 	id = fork();
 	if (id == -1)
-	{
-		perror("zsh");
-		return 1;
-	}
+		exit_error();
 	if (id == 0)
 		child_process(fd, av, envp);
 	else
 		parent_process(fd, av, envp);
 	wait(NULL);
-	return 0;
+	return (0);
 }
