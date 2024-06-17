5db58f097faad0fb63a632ca8bf8d7e7a36dcb7c
leoagea <leoagea@gmail.com>
2024-06-17 12:57:30 +0200

    Add : file and functs exit_malloc & exit error & free_arr

---
+++ src/exit.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 12:45:14 by lagea             #+#    #+#             */
+/*   Updated: 2024/06/17 12:54:23 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/pipex.h"
+
+void exit_malloc(void)
+{
+	write(2, "Error memory allocation\n", 24);
+	exit(1);
+}
+
+void exit_error(void)
+{
+	perror("zsh");
+	exit(1);
+}
+
+void free_arr(char **arr)
+{
+	int i = -1;
+	if (!arr)
+		return ;
+	while (arr[++i])
+		free(arr[i]);
+	free(arr);
+}
