e99ec176975ee335153ad4abde6ba02a39a9c5b4
leoagea <leoagea@gmail.com>
2024-06-17 14:58:24 +0200

    Fix : Norme OK

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 17:04:03 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/17 14:45:07 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/17 14:58:10 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@
 /*======================SRC=====================*/
 /*--------------------process-------------------*/
 
-void child_process(int *fd, char **av, char **envp);
-void parent_process(int *fd, char **av, char **envp);
+void	child_process(int *fd, char **av, char **envp);
+void	parent_process(int *fd, char **av, char **envp);
 
 /*---------------------exit---------------------*/
 
-void exit_malloc(void);
-void exit_error(void);
-void free_arr(char **arr);
+void	exit_malloc(void);
+void	exit_error(void);
+void	free_arr(char **arr);
 
 #endif
