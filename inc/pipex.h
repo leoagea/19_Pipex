cd2fdef3c3da2c5175ce577eab379ae25c77b8db
leoagea <leoagea@gmail.com>
2024-06-18 13:08:20 +0200

    Add : ft_free funct

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 17:04:03 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/17 14:58:10 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/18 13:05:35 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void	parent_process(int *fd, char **av, char **envp);
 
 void	exit_malloc(void);
 void	exit_error(void);
+void	ft_free(char **str);
 void	free_arr(char **arr);
 
 #endif
